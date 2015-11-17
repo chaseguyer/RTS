cf424399a89c8e029c7fcb83931eea53896c93db
chaseguyer <chaseguyer@gmail.com>
2015-11-17 15:21:47 -0800

    menu system stuff

--- Java Project/core/src/com/mygdx/game/RTS.java
+++ Java Project/core/src/com/mygdx/game/RTS.java
@@ -1,6 +1,9 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
