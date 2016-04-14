6724e8b2f2e487baa8cc010aaee146a0e22aa3a3
Kendall Gregory <kenrgregory112358@gmail.com>
2016-04-14 10:05:04 -0700

    asd

--- Java Project/core/src/com/mygdx/game/MazeGame.java
+++ Java Project/core/src/com/mygdx/game/MazeGame.java
@@ -1,34 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import java.util.Random;
 
 /**
  *
- * @author Kenny
+ * @author muel2767
+ * leave cards flipped for 1-5 seconds
+ * vertical/horizontal/diagonal 
+ * background solid or designed 
  */
-public class MazeGame extends ApplicationAdapter implements Screen, InputProcessor {
+public class MazeGame extends ApplicationAdapter implements Screen, InputProcessor
+{
     
-    private Maze maze;
-    private SpriteBatch batch;
-    
-    public MazeGame () {
-        maze = new Maze(10,10);
-    }
     
     @Override
-    public void show() {
+    public void show ()
+    {
         Gdx.graphics.setDisplayMode(Gdx.graphics.getDesktopDisplayMode().width, Gdx.graphics.getDesktopDisplayMode().height, true);
         Gdx.input.setInputProcessor(this);
 
@@ -36,14 +35,8 @@ public class MazeGame extends ApplicationAdapter implements Screen, InputProcess
     }
 
     @Override
-    public void render(float f) {
-        Gdx.gl.glClearColor(0,0,0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-        maze.draw(batch);
-    }
-
-    @Override
-    public void hide() {
+    public void render (float f) 
+    {
         
     }
 
@@ -86,5 +79,8 @@ public class MazeGame extends ApplicationAdapter implements Screen, InputProcess
     public boolean scrolled(int i) {
         return false;
     }
-    
+
+    @Override
+    public void hide() {
+    }
 }
