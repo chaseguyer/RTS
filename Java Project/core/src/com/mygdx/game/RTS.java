ed6adb4a9e41eacafd432ecce52d66ecfac88af2
chaseguyer <chaseguyer@gmail.com>
2016-04-26 13:49:54 -0600

    updated the file system

--- Java Project/core/src/com/mygdx/game/RTS.java
+++ Java Project/core/src/com/mygdx/game/RTS.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import java.io.File;
 
 /**
  *
@@ -16,6 +17,13 @@ public class RTS extends Game {
     public void create() {
         Gdx.graphics.setDisplayMode(Gdx.graphics.getDesktopDisplayMode().width, Gdx.graphics.getDesktopDisplayMode().height, true);        
 	//setScreen(menu);
+        
+        // create the folder that all the information for this game will go in
+        File dir = new File("RTS Data/patients");
+        dir.mkdirs();
+        dir = new File("RTS Data/therapists");
+        dir.mkdirs();
+        
         setScreen(new MainMenu());
     }
     
